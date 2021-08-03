75ec02242aaf1685088bd418a63ede521276501a
João Victor <santosjoao.dev@gmail.com>
2021-08-03 20:59:31 -0300

    applying eslint patterns

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,5 +1,5 @@
-import axios from "axios";
-import cheerio from "cheerio";
+import axios from 'axios';
+import cheerio from 'cheerio';
 
 export type dicionarioResponseProps = {
   palavra: string;
@@ -9,7 +9,7 @@ export type dicionarioResponseProps = {
 };
 
 export async function scrapeMeaning(
-  wordSearch: string
+  wordSearch: string,
 ): Promise<dicionarioResponseProps> {
   const { data } = await axios.get(`https://www.dicio.com.br/${wordSearch}/`);
   const $ = cheerio.load(data);
@@ -17,26 +17,28 @@ export async function scrapeMeaning(
     palavra: wordSearch,
     significado: [],
     etimologia: [],
-    error: "",
+    error: '',
   } as dicionarioResponseProps;
-  if (!$(".significado")) {
-    const error = { error: "A palavra não existe" } as dicionarioResponseProps;
+  if (!$('.significado')) {
+    const error = { error: 'A palavra não existe' } as dicionarioResponseProps;
     return error;
   }
 
-  $(".significado > span").each((index: number, element: cheerio.Element) => {
+  $('.significado > span').each((index: number, element: cheerio.Element) => {
     const elementValue = $(element).text();
     const elementHtml = $(element);
 
-    if (!elementHtml.hasClass("etim") && !elementHtml.hasClass("cl")) {
-      //@ts-ignore
+    if (!elementHtml.hasClass('etim') && !elementHtml.hasClass('cl')) {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
       dicionarioResponse.significado.push(elementValue);
     }
 
-    if (elementHtml.hasClass("etim")) {
+    if (elementHtml.hasClass('etim')) {
       dicionarioResponse.etimologia.push(
-        //@ts-ignore
-        elementValue.split(".")[1].replace(" ", "")
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        elementValue.split('.')[1].replace(' ', ''),
       );
     }
   });
